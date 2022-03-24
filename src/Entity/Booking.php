1933f0da4f474d4a4643a2dda17c2725dbf783ef
Tac Tacelosky <tacman@gmail.com>
2022-03-24 07:21:48 -0400

    install symfony

---
+++ src/Entity/Booking.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookingRepository::class)]
+#[ApiResource]
+class Booking
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    private $beginAt;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $endAt;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBeginAt(): ?\DateTimeImmutable
+    {
+        return $this->beginAt;
+    }
+
+    public function setBeginAt(\DateTimeImmutable $beginAt): self
+    {
+        $this->beginAt = $beginAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(?\DateTimeImmutable $endAt): self
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
